e69c90902551409872a94f3b4899e5a420589de0
zhouhu <zhouhu690109@163.com>
2016-06-05 03:59:48 +0800

    Add core and service

---
+++ fishing-service/src/main/java/org/fishing/service/App.java
@@ -0,0 +1,13 @@
+package org.fishing.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
